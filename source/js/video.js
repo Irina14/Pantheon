5c2113ea684a4b4b8cef868cb31b3c4993a6435f
Irina14 <ira1786@bk.ru>
2019-04-17 19:46:49 +0700

    add video in section Questions

--- source/js/video.js
+++ source/js/video.js
@@ -1,34 +1,51 @@
 'use strict';
 
 (function () {
-  var URL = 'https://www.youtube.com/embed/nvl2qnYVBM8';
-  var video = document.querySelector('.video__wrapper');
-  var videoLink = document.querySelector('.video__link');
-  var videoButton = document.querySelector('.video__button');
+  var URLS = ['https://www.youtube.com/embed/nvl2qnYVBM8', 'https://www.youtube.com/embed/TBxE8FkDb-0'];
+  var videoPantheon = document.querySelector('.video__wrapper--pantheon');
+  var videoLinkPantheon = videoPantheon.querySelector('.video__link');
+  var videoButtonPantheon = videoPantheon.querySelector('.video__button');
+  var videoQuestions = document.querySelector('.video__wrapper--questions');
+  var videoLinkQuestions = videoQuestions.querySelector('.video__link');
+  var videoButtonQuestions = videoQuestions.querySelector('.video__button');
+  var videos = document.querySelectorAll('.video__wrapper');
+  var videoLinks = document.querySelectorAll('.video__link');
 
-  video.addEventListener('click', function () {
-    var iframe = createIframe();
-    videoLink.remove();
-    videoButton.remove();
-    video.appendChild(iframe);
+  videoPantheon.addEventListener('click', function () {
+    var iframe = createIframe(URLS[0]);
+    videoLinkPantheon.remove();
+    videoButtonPantheon.remove();
+    videoPantheon.appendChild(iframe);
   });
 
-  videoLink.removeAttribute('href');
-  video.classList.add('video--enabled');
+  videoQuestions.addEventListener('click', function () {
+    var iframe = createIframe(URLS[1]);
+    videoLinkQuestions.remove();
+    videoButtonQuestions.remove();
+    videoQuestions.appendChild(iframe);
+  });
 
-  var createIframe = function () {
+  var generateURL = function (url) {
+    var QUERY = '?rel=0&showinfo=0&autoplay=1'; // видео сразу запускается
+    return url + QUERY;
+  }
+
+  var createIframe = function (url) {
     var iframe = document.createElement('iframe');
     iframe.setAttribute('width', '100%');
     iframe.setAttribute('height', '100%');
-    iframe.setAttribute('src', generateURL(URL));
+    iframe.setAttribute('src', generateURL(url));
     iframe.setAttribute('frameborder', '0');
     iframe.setAttribute('allow', 'autoplay');
     iframe.setAttribute('allowfullscreen', '');
     return iframe;
   };
 
-  var generateURL = function (url) {
-    var QUERY = '?rel=0&showinfo=0&autoplay=1'; // видео сразу запускается
-    return url + QUERY;
+  for (var i = 0; i < videos.length; i++) {
+    videos[i].classList.add('video--enabled');
+  }
+
+  for (var i = 0; i < videoLinks.length; i++) {
+    videoLinks[i].removeAttribute('href');
   }
 })();
